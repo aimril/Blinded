9bfe5f4634cad3918308e1f2909f52cb13f14705
Erwin Umali <kazukishida@gmail.com>
2013-12-02 00:40:50 +0800

    fix move forward with space key

--- Assets/Julius.cs
+++ Assets/Julius.cs
@@ -2,34 +2,45 @@
 using System.Collections;
 
 public class Julius : MonoBehaviour {
-
+	
+	public float moveSpeed = 3.0f;
+	public int rotateSpeed = 90;
 	private float smooth = 2.0f;
 	int x = 0;
 
+	private CharacterController controller;
+	
+	void Start () {
+		controller = (CharacterController) GetComponent("CharacterController");
+	}
 	
 	void Update () {
 		
 		var target = Quaternion.Euler (0, x, 0);
 		
 		if(Input.GetKeyDown(KeyCode.D)){
-			x += 90;
+			x += rotateSpeed;
 			target = Quaternion.Euler (0, x, 0);
 			Debug.Log(x+ " is the value of x.");
 		}
 		else if(Input.GetKeyDown(KeyCode.A)){
-			x -= 90;
+			x -= rotateSpeed;
 			target = Quaternion.Euler (0, x, 0);
 			Debug.Log(x+ " is the value of x.");
 			
 		}
 		
 		if(Input.GetKey(KeyCode.Space)){
-		
 			//rigidbody.AddForce(Vector3.back);
 			//transform.Translate(0,0,5*Time.deltaTime);
+			Debug.Log ("rigidbody rotation = " + rigidbody.rotation);
+			//gameObject.rigidbody.AddForce(rigidbody.rotation * Vector3.forward * moveSpeed);
+			controller.Move (rigidbody.rotation * Vector3.forward * moveSpeed * Time.deltaTime);
 
+		} else if(Input.GetKey(KeyCode.S)){
+			controller.Move (rigidbody.rotation * Vector3.back * moveSpeed * Time.deltaTime);
 			
-		}		
+		}
 		
 	
 	// Apply the direction to the CharacterMotor
